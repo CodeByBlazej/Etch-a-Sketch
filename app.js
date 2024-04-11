7affd8711b08c275cb737adf6233299aa72af650
CodeByBlazej <codebyblazej@gmail.com>
2024-04-11 21:45:33 +0100

    make 16x16 grid

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ const container = document.querySelector(".container");
 
 
 function createDivs() {
-  for(let newDiv = 0; newDiv <= 16; newDiv++) {
-    let newDiv = document.createElement("div");
-    newDiv.style.borderColor = 'black';
-    container.appendChild(newDiv);
+  for(let y = 1; y <= 16; y++) {
+    
+    let newDivX = document.createElement("div");
+    newDivX.className = "divX";
 
+    newDivX.style.cssText = 'display: flex; background-color: yellow; gap: 20px;';
+    container.appendChild(newDivX);
+
+    for(let newDiv = 1; newDiv <= 16; newDiv++) {
+      let newDiv = document.createElement("div");
+      newDiv.style.cssText =
+        'display:flex; flex-direction: column; height: 10px; width: 10px; background-color:red; gap: 20px;';
+      newDivX.appendChild(newDiv);
+    }
   }
 }
 
