48659de955ed1a9d71e02be4e41cbda0cb29fd93
CodeByBlazej <codebyblazej@gmail.com>
2024-04-12 19:24:10 +0100

    remove unnecessary gap and flex

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ function createDivs() {
     let newDivX = document.createElement("div");
     newDivX.className = "divX";
 
-    newDivX.style.cssText = 'display: flex; background-color: yellow; gap: 20px;';
+    newDivX.style.cssText = 'display: flex; background-color: yellow; gap: 20px ';
     container.appendChild(newDivX);
 
     for(let newDiv = 1; newDiv <= 16; newDiv++) {
       let newDiv = document.createElement("div");
       newDiv.style.cssText =
-        'display:flex; flex-direction: column; height: 10px; width: 10px; background-color:red; gap: 20px;';
+        'display:flex; height: 20px; width: 20px; background-color:red;';
       newDivX.appendChild(newDiv);
     }
   }
