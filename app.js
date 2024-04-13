e7ef28121b8847098b48f9794359307e5b7b59a4
CodeByBlazej <codebyblazej@gmail.com>
2024-04-13 21:41:40 +0100

    flex grid inside the container with demanded grid

--- app.js
+++ app.js
@@ -1,48 +1,50 @@
 const container = document.querySelector(".container");
+const btn = document.querySelector(".btn");
 
 
-function createButton() {
-  let btn = document.createElement("button");
+function createGrid(question) {
   
-  let parentDiv = container.parentNode;
-  parentDiv.insertBefore(btn, container)
-  btn.textContent = 'Choose the grid!';
-  btn.style.cssText = 'margin: 20px; background-color: lightblue;'
-}
-
-
-createButton();
-
-
-function createDivs() {
-
-
-  for(let y = 1; y <= 16; y++) {
+  
+  for(let y = 1; y <= question; y++) {
     
     let newDivY = document.createElement("div");
     newDivY.className = "divY";
 
-    newDivY.style.cssText = 'display: flex; background-color: yellow; gap: 20px ';
+    newDivY.style.cssText = 'display: flex; flex-grow: 1; justify-content: center; background-color: yellow;  ';
     container.appendChild(newDivY);
 
-    for(let x = 1; x <= 16; x++) {
+    for(let x = 1; x <= question; x++) {
       let newDivX = document.createElement("div");
       newDivX.className = "divX";
 
       newDivX.style.cssText =
-        'display:flex; height: 20px; width: 20px; background-color:red;';
+      'display:flex; flex-grow: 1;  background-color:red;';
       
       
       newDivX.addEventListener('mouseover', () => {
           newDivX.style.backgroundColor = "pink";
         });
 
-      newDivY.appendChild(newDivX);
+        newDivY.appendChild(newDivX);
+      }
     }
   }
-}
-
-createDivs();
-
-
+  
+  createGrid(16);
+  
+  
+  
+  btn.addEventListener('click', () => {
+    let question = prompt('How many squeres per side would you like your grid to be?');
+    question = question.toUpperCase();
+  
+    if(question <= 0 || question > 100) {
+      alert('You entered too small or too big value! Try again with numbers from 1 to 100.');
+    } else {
+      container.textContent = "";
+      createGrid(question);
+    }
+  
+  })
+  
   
