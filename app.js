dbb1c945bb07bac85d54911e5c824076336a3356
CodeByBlazej <codebyblazej@gmail.com>
2024-04-13 13:53:27 +0100

    add hover to divs and button to choose grid

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
 const container = document.querySelector(".container");
 
 
+function createButton() {
+  let btn = document.createElement("button");
+  
+  let parentDiv = container.parentNode;
+  parentDiv.insertBefore(btn, container)
+  btn.textContent = 'Choose the grid!';
+  btn.style.cssText = 'margin: 20px; background-color: lightblue;'
+}
+
+
+createButton();
+
+
 function createDivs() {
+
+
   for(let y = 1; y <= 16; y++) {
     
-    let newDivX = document.createElement("div");
-    newDivX.className = "divX";
+    let newDivY = document.createElement("div");
+    newDivY.className = "divY";
 
-    newDivX.style.cssText = 'display: flex; background-color: yellow; gap: 20px ';
-    container.appendChild(newDivX);
+    newDivY.style.cssText = 'display: flex; background-color: yellow; gap: 20px ';
+    container.appendChild(newDivY);
 
-    for(let newDiv = 1; newDiv <= 16; newDiv++) {
-      let newDiv = document.createElement("div");
-      newDiv.style.cssText =
+    for(let x = 1; x <= 16; x++) {
+      let newDivX = document.createElement("div");
+      newDivX.className = "divX";
+
+      newDivX.style.cssText =
         'display:flex; height: 20px; width: 20px; background-color:red;';
-      newDivX.appendChild(newDiv);
+      
+      
+      newDivX.addEventListener('mouseover', () => {
+          newDivX.style.backgroundColor = "pink";
+        });
+
+      newDivY.appendChild(newDivX);
     }
   }
 }
@@ -22,3 +45,4 @@ function createDivs() {
 createDivs();
 
 
+  
